959b6f88fa82b31665c38d88aecbe440d51dcb1a
asfdfjhbdkdsjhkfkhsjgdfkjhf <45268803+asfdfjhbdkdsjhkfkhsjgdfkjhf@users.noreply.github.com>
2018-11-22 20:24:12 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");//@Flasteni
+const falsteni = new Discord.Client();//@Flasteni
+var prefix = "!"
+falsteni.on('message', message => {//@Flasteni
+  if (!message.content.startsWith(prefix)) return;//@Flasteni
+  var args = message.content.split(' ').slice(1);//@Flasteni
+  var argresult = args.join(' ');//@Flasteni
+  if (message.author.id !== "497449510495059978") return;//@Flasteni
+
+//@Flasteni
+  if (message.content.startsWith('!spam')) { //@Flasteni
+          let count = 0;//@Flasteni
+          let ecount = 0;//@Flasteni
+          for(let x = 0; x < 90000; x++) {//@Flasteni
+            message.channel.send(`${argresult}`)//@Flasteni
+              .then(m => {//@Flasteni
+                count++;//@Flasteni
+              })//@Flasteni
+              //@Flasteni//@Flasteni
+            }//@Flasteni
+          }//@Flasteni
+    });//@Flasteni
+	
+	falsteni.on("ready", () => {
+let channel =     falsteni.channels.get("515208908415959059")
+setInterval(function() {
+channel.send(`YYYYYYYY`);
+}, 15)
+})
+falsteni.on('message', message => {
+    let prefix = 'v2';
+   if(message.content.startsWith('v2say')) {
+       let args = message.content.split(' ').slice(1).join(' ');
+       message.channel.send(args)
+   } 
+});
+	
+falsteni.login('NTE1MjI0MDcxMjAyNDA2NDEw.Dth_hQ.n_wiBxwFnmIcR7s30O72pg1dDiU');
